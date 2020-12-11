0ecd2f0574fa1d52a8818cb8e00b02e83e4b48c2
shernk <31926953+shernk@users.noreply.github.com>
2020-12-12 02:04:19 +0700

    access data

--- server.js
+++ server.js
@@ -1,27 +1,109 @@
-const express = require("express");
-const { expressGraphQL } = require("express-graphql");
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLString,
+  GraphQLInt,
+} = require("graphql");
+
 const app = express();
 
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: "HelloWorld",
-    fields: () => ({
-      message: {
-        type: GraphQLString,
-        resolve: () => "Hello World",
-      },
-    }),
+const authors = [
+  {
+    id: 1,
+    name: "sherkname",
+  },
+  {
+    id: 2,
+    name: "Christopher",
+  },
+  {
+    id: 3,
+    name: "Alking",
+  },
+];
+const books = [
+  {
+    id: 1,
+    name: "Harry Potter and the Chamber of Secrets",
+    authorId: 1,
+  },
+  {
+    id: 2,
+    name: "Harry Potter and the Prisoner of Azkaban",
+    authorId: 2,
+  },
+  {
+    id: 3,
+    name: "Harry Potter and the Goblet of Fire",
+    authorId: 3,
+  },
+  {
+    id: 4,
+    name: "The Way of Shadows",
+    authorId: 3,
+  },
+];
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  description: "This represents a author of a book",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
   }),
+});
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  description: "This represents a book written by an author",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    authorId: { type: GraphQLNonNull(GraphQLInt) },
+    author: {
+      type: AuthorType,
+      resolve: (book) => {
+        return authors.find((author) => book.authorId === author.id);
+      },
+    },
+  }),
+});
+
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  description: "Root Query",
+  fields: () => ({
+    books: {
+      type: new GraphQLList(BookType),
+      description: "List of All Books",
+      resolve: () => books,
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      description: "List of All Authors",
+      resolve: () => authors,
+    },
+  }),
+});
+
+const schema = new GraphQLSchema({
+  query: RootQueryType,
 });
 
 app.use(
   "/graphql",
-  expressGraphQL({
+  graphqlHTTP({
     schema: schema,
     graphiql: true,
   })
 );
 
-app.listen(5000, () => console.log("Server Running"));
+app.listen(4000, () => console.log("Server Running"));
